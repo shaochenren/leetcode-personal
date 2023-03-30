2477710234a21e8de1c4d51f9eb8d7eac512980e
shaochenren <46946162+shaochenren@users.noreply.github.com>
2023-03-29 23:42:41 -0400

    Update #二分法算法 默写 time The time complexity of Bi.py

--- #二分法算法 默写 time The time complexity of Bi.py
+++ #二分法算法 默写 time The time complexity of Bi.py
@@ -1,15 +1,14 @@
 #二分法算法 默写  time The time complexity of Binary Search Algorithm is O(logN) (Where n is the number of elements in the sorted linear array.)
 def binarysearch(self,num,start,end,target):
     mid = (start+end)//2
-    if not num:
-        return False
     if start >=end:
-        return False
+        return False #(sometimes return start, sometimes return end)
     if num[mid] == target:
         return mid
     if target > num[mid]:
         return binarysearch(self,num,mid+1,end,target)
     else:
         return binarysearch(self,num,start,mid+1,target)
+    # if we use two function, we need to return something here
 
     
